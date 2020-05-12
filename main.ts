f96572a1d2f8e1b755443f89b33a51f2123d7ea2
calvinsk <65216949+calvinsk@users.noreply.github.com>
2020-05-12 16:36:01 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,83 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2512
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\fswiss\fcharset0 Helvetica;\f2\fnil\fcharset136 PingFangHK-Regular;
+}
+{\colortbl;\red255\green255\blue255;\red25\green25\blue25;\red0\green0\blue0;\red0\green0\blue0;
+}
+{\*\expandedcolortbl;;\cssrgb\c12941\c12941\c12941;\cssrgb\c0\c0\c0\c5882;\cssrgb\c0\c0\c0;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+
+\itap1\trowd \taflags4 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil \tapadt360 \tapadb360 
+\clvertalc \clshdrawnil \clwWidth28620\clftsWidth3 \clmart10 \clmarl10 \clmarb10 \clmarr10 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
+\pard\intbl\itap1\pardeftab720\sl420\partightenfactor0
+
+\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 //% weight=0 color=#3CB371 icon="\\uf0ad" block="Tools"
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3 namespace tools \{
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     /**
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     * 
+\f2 \'ad\'70\'ba\'e2\'aa\'f8\'a4\'e8\'a7\'ce\'ad\'b1\'bf\'6e\'a1\'41\'a8\'c3\'a6\'5e\'b6\'c7
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     */
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     //% blockGap=2 weight=0 blockExternalInputs=true
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     export function areaOfRectangle(length: number, width:number): number \{
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3         return length*width
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     \}
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     /**
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     * 
+\f2 \'ad\'70\'ba\'e2\'aa\'f8\'a4\'e8\'a7\'ce\'ad\'b1\'bf\'6e\'a1\'41\'a4\'a3\'a6\'5e\'b6\'c7\'a1\'41\'a5\'75\'c5\'e3\'a5\'dc\'a6\'62
+\f0 LED
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     */
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     //% blockGap=2 weight=1
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     export function ledOfRectangle(length: number, width:number): void \{
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3         basic.showNumber(length*width)
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3     \}
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \
+
+\f0 \AppleTypeServices \cb3 \}
+\f1 \AppleTypeServices\AppleTypeServicesF65539 \cb1 \cell \lastrow\row
+\pard\pardeftab720\partightenfactor0
+
+\fs32 \AppleTypeServices \cf4 \strokec4 \
+
+\itap1\trowd \taflags4 \trgaph108\trleft-108 \trbrdrt\brdrnil \trbrdrl\brdrnil \trbrdrt\brdrnil \trbrdrr\brdrnil \tapadt360 \tapadb360 
+\clvertalc \clshdrawnil \clwWidth28620\clftsWidth3 \clmart10 \clmarl10 \clmarb10 \clmarr10 \clbrdrt\brdrnil \clbrdrl\brdrnil \clbrdrb\brdrnil \clbrdrr\brdrnil \clpadl0 \clpadr0 \gaph\cellx8640
+\pard\intbl\itap1\pardeftab720\partightenfactor0
+\cf4 \cell \lastrow\row
+}
